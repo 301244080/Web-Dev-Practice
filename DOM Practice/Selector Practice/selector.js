872479ca01a4ff5556f2cbb0c9fc0015090ec553
Andrew <yy9577@gmail.com>
2018-04-13 19:29:00 -0700

    Finished simple DOM selector exercise

---
+++ DOM Practice/Selector Practice/selector.js
@@ -0,0 +1,8 @@
+// Try selecting the first p tag
+
+var sel1 = document.querySelector("#first");
+// val sel2 = document.querySelectorAll(".special")[0];
+var sel3 = document.getElementById("first");
+var sel4 = document.getElementsByClassName("special")[0];
+var sel5 = document.getElementsByTagName("p")[0];
+var sel6 = document.querySelector("h1 + p");
